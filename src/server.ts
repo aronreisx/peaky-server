d2186fbf11a57ca4b216f31d449232fdd147c90a
aronreisx <aronreis2@gmail.com>
2021-12-09 00:19:09 +0000

    :sparkles: Add Router middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
-require('dotenv').config()
+import express from "express";
+
+import routes from "./routes";
+
 const { SERVER_PORT } = process.env;
 
 import express from 'express';
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(SERVER_PORT, () => {
   console.log(`[server] Server started on PORT ${SERVER_PORT}`)
