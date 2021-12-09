18afa7b743b5a02db54f5495cb1a99dfd9e62473
aronreisx <aronreis2@gmail.com>
2021-12-09 04:02:14 +0000

    :sparkles: Add route get

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 
+import AppointmentsRepository from "../repositories/AppointmentsRepository";
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
 appointmentsRouter.get("/", (req, res) => {
-  return res.json({ message: "hello" });
+  const appointments = appointmentsRepository.all();
+
+  return res.json(appointments);
+});
+
 });
 
 export default appointmentsRouter;
