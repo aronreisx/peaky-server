36091d07860f7189f50224aca1000d444386424f
aronreisx <aronreis2@gmail.com>
2021-12-11 09:59:00 +0000

    :recycle: Update Get and Post methods

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,15 +1,36 @@
+import { parseISO } from "date-fns";
 import { Router } from "express";
+import { getCustomRepository } from "typeorm";
 
 import AppointmentsRepository from "../repositories/AppointmentsRepository";
-const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
+import CreateAppointmentService from "../services/CreateAppointmentService";
 
-appointmentsRouter.get("/", (req, res) => {
-  const appointments = appointmentsRepository.all();
+const appointmentsRouter = Router();
+
+appointmentsRouter.get("/", async (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return res.json(appointments);
 });
 
+appointmentsRouter.post("/", async (req, res) => {
+  try {
+    const { provider, date } = req.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return res.json(appointment);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
 });
 
 export default appointmentsRouter;
