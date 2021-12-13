da3b2191138bd3c0b2c196a341408757ce117c36
aronreisx <aronreis2@gmail.com>
2021-12-13 19:07:06 +0000

    :pencil2: Fix typos

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -5,12 +5,12 @@ import Appointment from "../models/Appointment";
 import AppointmentsRepository from "../repositories/AppointmentsRepository";
 
 interface IRequest {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ date, provider }: IRequest): Promise<Appointment> {
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(date);
@@ -24,7 +24,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
