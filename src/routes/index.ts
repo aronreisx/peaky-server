3885412a2c93cc3700dcf93d2e7c78b4778e60a1
aronreisx <aronreis2@gmail.com>
2021-12-15 22:56:08 +0000

    ✨ Update route Index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import appointmentsRouter from "./appointments.routes";
+import sessionsRouter from "./sessions.routes";
 import usersRouter from "./users.routes";
 
 const routes = Router();
 
 routes.use("/appointments", appointmentsRouter);
+routes.use("/sessions", sessionsRouter);
 routes.use("/users", usersRouter);
 
 export default routes;
