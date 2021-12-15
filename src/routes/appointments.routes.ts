951b216015a851590f82f1459cc819b693401dee
aronreisx <aronreis2@gmail.com>
2021-12-15 22:25:11 +0000

    ✨ Update route Appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -2,10 +2,13 @@ import { parseISO } from "date-fns";
 import { Router } from "express";
 import { getCustomRepository } from "typeorm";
 
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 import AppointmentsRepository from "../repositories/AppointmentsRepository";
 import CreateAppointmentService from "../services/CreateAppointmentService";
 
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get("/", async (req, res) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
