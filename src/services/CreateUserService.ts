92412cdd25436e41a67dc52583808d5e647abbb2
aronreisx <aronreis2@gmail.com>
2021-12-15 22:38:55 +0000

    ✨ Add service CreateUserService

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import { hash } from "argon2";
+import { getRepository } from "typeorm";
+
+import User from "../models/User";
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new Error("Email address is already used.");
+    }
+
+    const hashedPassword = await hash(password);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
