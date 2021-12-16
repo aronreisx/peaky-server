8af1325ca7ab614d51900837631a1c1a94b01ecd
aronreisx <aronreis2@gmail.com>
2021-12-16 04:35:31 +0000

    ♻️ Remove all try/catches from route modules

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,23 +5,19 @@ import AuthenticateUserService from "../services/AuthenticateUserService";
 const sessionsRouter = Router();
 
 sessionsRouter.post("/", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user, token } = await authenticateUser.execute({
-      email,
-      password,
-    });
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
 
-    const authenticatedUser = Object.assign(user);
-    delete authenticatedUser.password;
+  const authenticatedUser = Object.assign(user);
+  delete authenticatedUser.password;
 
-    return res.json({ authenticatedUser, token });
-  } catch (err) {
-    return res.status(err.statusCode).json({ error: err.message });
-  }
+  return res.json({ authenticatedUser, token });
 });
 
 export default sessionsRouter;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -10,26 +10,22 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post("/", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    const createdUser = Object.assign(Object.create(user), {
-      name,
-      email,
-    });
+  const createdUser = Object.assign(Object.create(user), {
+    name,
+    email,
+  });
 
-    return res.json(createdUser);
-  } catch (err) {
-    return res.status(400).json({ error: err.message });
-  }
+  return res.json(createdUser);
 });
 
 usersRouter.patch(
@@ -37,21 +33,17 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single("avatar"),
   async (req, res) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatar.execute({
-        user_id: req.user.id,
-        avatarFilename: req.file?.filename,
-      });
+    const user = await updateUserAvatar.execute({
+      user_id: req.user.id,
+      avatarFilename: req.file?.filename,
+    });
 
-      const updatedUser = Object.assign(user);
-      delete updatedUser.password;
+    const updatedUser = Object.assign(user);
+    delete updatedUser.password;
 
-      return res.json(updatedUser);
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
-    }
+    return res.json(updatedUser);
   }
 );
 
